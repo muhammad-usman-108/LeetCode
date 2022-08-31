38933df361a09b9a6a3806b683605810ba8700fb
Muhammad Usman <34769369+muhammad-usman-108@users.noreply.github.com>
2022-08-31 12:02:39 +0200

    Time: 86 ms (8.36%), Space: 14.1 MB (31.85%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t = sorted(t)
+        s = sorted(s)
+        print(t , " ", s)
+        if (len(t) == (len(s) + 1)):
+            for i in range(0, len(t)-1):
+                if t[i] != s[i]:
+                    return t[i]
+            return t[len(t)-1]
+        else:
+            return ""
