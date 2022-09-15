763970236365531bc3e934ab0d40227c15c69c30
Muhammad Usman <34769369+muhammad-usman-108@users.noreply.github.com>
2022-09-15 18:00:18 +0200

    Time: 1723 ms (5.09%), Space: 204.2 MB (5.09%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int[] original = new int[changed.length/2];
+        if (changed.length%2 == 1) {
+            return new int[0];
+        }
+        
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        
+        for (int i=0; i<changed.length; i++) {
+            if(temp.containsKey(changed[i])) {
+                temp.put(changed[i], temp.get(changed[i]) + 1);
+            } else {
+                temp.put(changed[i], 1);
+            }
+        }
+        System.out.println(temp);
+        Arrays.sort(changed);
+        int index = 0;
+        for (int i=0; i<changed.length; i++) {
+            if(temp.containsKey(changed[i]*2)) {
+                if (changed[i] == 0) {
+                    if (temp.get(changed[i]) > 1) {
+                        temp.put(changed[i], temp.get(changed[i]) - 2);
+                        original[index] = changed[i];
+                        System.out.println(index);
+                        index++;
+                    }
+                } else {
+                    if (temp.get(changed[i]*2) > 0 && temp.get(changed[i]) > 0) {
+                        temp.put(changed[i]*2, temp.get(changed[i]*2) - 1);
+                        temp.put(changed[i], temp.get(changed[i]) - 1);
+                        original[index] = changed[i];
+                        System.out.println(index);
+                        index++;
+                    }
+                }
+                
+                if (changed.length/2 == index) {
+                    return original;
+                }
+            }
+        }
+        
+        
+        
+        return new int[0];
+    }
+}
